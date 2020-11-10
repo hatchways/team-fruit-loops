e8769ddc16d845d8dcf82ce68543a71ce69fd335
AltiV <vuong.alan.t@gmail.com>
2020-11-10 14:57:46 -0500

    Preliminary login and signup page creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
+import { MuiThemeProvider, CssBaseline, Toolbar } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
+
+import Navbar from "./pages/Navbar";
 import LandingPage from "./pages/Landing";
 import Chat from "./pages/Chat.js";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 import "./App.css";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <Toolbar />
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
         <Route path="/chat" component={Chat}/>
       </BrowserRouter>
     </MuiThemeProvider>
