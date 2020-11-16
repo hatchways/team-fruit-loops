6be311b53d6148d56664fab5379576cbaa85253b
Travis Matthews <trav.matth@gmail.com>
2020-11-16 11:50:33 -0800

    chat mvp

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import Chat from "./pages/Chat.js";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <Route path="/" component={LandingPage} />
+        <Route path="/chat" component={Chat}/>
       </BrowserRouter>
     </MuiThemeProvider>
   );
