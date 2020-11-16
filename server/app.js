2252e367371372065b731d2519c03ff6372dc07f
Travis Matthews <trav.matth@gmail.com>
2020-11-16 13:44:37 -0800

    extract mongoose connection to separate file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
+require("./database/db");
 const createError = require("http-errors");
 const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = require('mongoose');
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
@@ -11,24 +11,7 @@ const gameRouter = require("./routes/game");
 
 const { json, urlencoded } = express;
 
-const mongoURL = process.env.MONGODB_URL || "";
 const app = express();
-
-// Connect to MongoDB
-mongoose.connect(mongoURL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true
-})
-.catch(error => {
-  console.log(`Error connecting to MongoDB: ${error}`);
-  process.exit(1);
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
 
 app.use(logger("dev"));
 app.use(json());
@@ -40,21 +23,6 @@ app.use("/", indexRouter);
 app.use("/ping", pingRouter);
 app.use('/game', gameRouter);
 
-// Connect to MongoDB
-const mongoURL = process.env.MONGODB_URL || "";
-mongoose.connect(mongoURL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true
-})
-.catch(error => {
-  console.log(`Error connecting to MongoDB: ${error}`);
-  process.exit(1);
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)));
 
