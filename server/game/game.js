67e092d314b4ec6aec3e28e29c5e70fd63a28a72
AltiV <vuong.alan.t@gmail.com>
2020-12-02 02:57:11 -0500

    Reverted isReady

--- server/game/game.js
+++ server/game/game.js
@@ -31,11 +31,10 @@ const initGame = () => {
 
 const isReady = (gameState) => {
   return (
-    // gameState.waitingList.length === 0 &&
-    gameState.blueSpy !== undefined 
-    // gameState.redSpy !== undefined &&
-    // gameState.blueGuessers.length !== 0 &&
-    // gameState.redGuessers.length !== 0
+    gameState.blueSpy !== undefined &&
+    gameState.redSpy !== undefined &&
+    gameState.blueGuessers.length !== 0 &&
+    gameState.redGuessers.length !== 0
   );
 }
 
