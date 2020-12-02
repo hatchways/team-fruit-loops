de768a710f8e7bee739d9090936ec46bae94e6ac
Travis Matthews <trav.matth@gmail.com>
2020-12-01 23:49:29 -0800

    heroku runs web command locally

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, "..", "client", "build")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
